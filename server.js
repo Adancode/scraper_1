27d35d068c13303436b0963e8d4f20019c24d733
Adam Camacho <adamcamacho8@gmail.com>
2016-08-26 19:53:17 -0500

    min requirements met, adding more features and CSS soon

---
+++ server.js
@@ -0,0 +1,34 @@
+/* Showing Mongoose's "Populated" Method (18.3.8)
+ * INSTRUCTOR ONLY
+ * =============================================== */
+
+// dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+/*var logger = require('morgan');*/
+
+// Notice: Our scraping tools are prepared, too
+
+var exphbs  = require('express-handlebars');
+
+// Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// use morgan and bodyparser with our app
+/*app.use(logger('dev'));*/
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// make public a static dir
+app.use(express.static('public'));
+
+var routes = require('./controllers/controller.js');
+app.use('/', routes);
+
+// listen on port 3000
+app.listen(3001, function() {
+  console.log('App running on port 3001!');
+});
